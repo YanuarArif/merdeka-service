7ccb3d0a3280f3b5c5ec87acba3b7099337fd017
Yanuar Lukman Arif <yanuarlukmanarif@gmail.com>
2025-01-03 22:33:44 +0700

    edit lain-lain 12

--- src/app/(authentication)/login/callback/route.ts
+++ src/app/(authentication)/login/callback/route.ts
@@ -6,28 +6,35 @@ export const dynamic = "force-dynamic"; // Ensures dynamic behavior
 export const revalidate = 0; // No caching
 
 export async function GET(request: Request) {
-  const { searchParams, origin } = new URL(request.url);
-  const code = searchParams.get("code");
-  // if "next" is in param, use it as the redirect URL
-  const next = searchParams.get("next") ?? "/";
+  try {
+    const { searchParams, origin } = new URL(request.url);
+    const code = searchParams.get("code");
+    // if "next" is in param, use it as the redirect URL
+    const next = searchParams.get("next") ?? "/";
 
-  if (code) {
-    const supabase = await createClient();
-    const { error } = await supabase.auth.exchangeCodeForSession(code);
-    if (!error) {
-      const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
-      const isLocalEnv = process.env.NODE_ENV === "development";
-      if (isLocalEnv) {
-        // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
-        return NextResponse.redirect(`${origin}${next}`);
-      } else if (forwardedHost) {
-        return NextResponse.redirect(`https://${forwardedHost}${next}`);
+    if (code) {
+      const supabase = await createClient();
+      const { error } = await supabase.auth.exchangeCodeForSession(code);
+      if (!error) {
+        const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
+        const isLocalEnv = process.env.NODE_ENV === "development";
+        if (isLocalEnv) {
+          // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
+          return NextResponse.redirect(`${origin}${next}`);
+        } else if (forwardedHost) {
+          return NextResponse.redirect(`https://${forwardedHost}${next}`);
+        } else {
+          return NextResponse.redirect(`${origin}`); // change from `${origin}${next}` to `${origin}`
+        }
       } else {
-        return NextResponse.redirect(`${origin}`); // change from `${origin}${next}` to `${origin}`
+        console.error("Supabase session exchange error:", error);
       }
     }
-  }
 
-  // return the user to an error page with instructions
-  return NextResponse.redirect(`${origin}/auth/auth-code-error`);
+    // If the code is not present or an error occurred, redirect to error page
+    return NextResponse.redirect(`${origin}/auth/auth-code-error`);
+  } catch (error) {
+    console.error("An unexpected error occurred:", error);
+    return NextResponse.redirect("/error?message=unexpected_error"); // Redirect to a general error page with an error message
+  }
 }
