d6131055342b11707faa422a3f5622020e21685a
Yanuar Lukman Arif <yanuarlukmanarif@gmail.com>
2025-02-11 15:23:36 +0700

    edit menu dropdown menjadi bg-none saat hover/sorot dengan pointer

--- src/components/ui/shadcnblocks-com-navbar1.tsx
+++ src/components/ui/shadcnblocks-com-navbar1.tsx
@@ -246,7 +246,7 @@ const renderMenuItem = (item: MenuItem) => {
   return (
     <a
       key={item.title}
-      className="group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium text-muted-foreground transition-colors hover:bg-muted hover:text-accent-foreground"
+      className="group inline-flex h-10 w-max items-center justify-center rounded-md bg-none px-4 py-2 text-sm font-medium text-muted-foreground transition-colors hover:bg-none hover:text-accent-foreground"
       href={item.url}
     >
       {item.title}
